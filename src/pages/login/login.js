4f59f80f61ab53bcedf7965455682e9c742192fe
Unknown <eckan@live.Fr>
2018-03-08 15:32:45 +0100

    feat: valid connect

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, Text, TextInput, TouchableHighlight, Image, Button, StyleSheet } from 'react-native'
-import {StackNavigator} from 'react-navigation'
+import { StackNavigator } from 'react-navigation'
+
+import * as firebase from "firebase";
 
 import '../login/register'
 
@@ -10,13 +12,33 @@ export default class Login extends React.Component {
         super(props)
         this.state = {
             pseudo: 'Pseudo',
-            password: 'Mot de passe'
+            password: 'Mot de passe',
+            users: [],
         }
     }
 
     console = () => {
-        console.log('hello')
+        console.log(this.state.pseudoId)
     }
+
+    connected = () => {
+        let recentPostsRef = firebase.database().ref('/users');
+
+        recentPostsRef.once('value').then(snapshot => { // get references de /user
+            userList = snapshot.val()
+            snapshot.forEach(user => { // boucle a travers les users en bases
+                var userFormated = user.val();
+                this.setState({
+                    users: userFormated
+                })
+                if (this.state.pseudo === userFormated.pseudo && this.state.password === userFormated.password) {
+                    console.log('nice')
+                    this.props.navigation.navigate('Home')
+                } 
+            })
+        })
+    }
+
 
     render() {
 
@@ -27,32 +49,32 @@ export default class Login extends React.Component {
                     style={styles.logoC}
                 />
                 <View style={styles.ConnectBox}>
-                <View style={{ flexDirection: 'column' }}>
-                    <Text style={{fontSize: 20, color: '#662680', marginTop: 6,}}> 
-                        Connexion 
+                    <View style={{ flexDirection: 'column' }}>
+                        <Text style={{ fontSize: 20, color: '#662680', marginTop: 6, }}>
+                            Connexion
                     </Text>
-                    <Image 
-                        source={require('../../img/trait-input.png')}
-                        style={{width: 95, height: 40, marginTop: -13,}}
-                    />
+                        <Image
+                            source={require('../../img/trait-input.png')}
+                            style={{ width: 95, height: 40, marginTop: -13, }}
+                        />
                     </View>
-                    <Image 
+                    <Image
                         source={require('../../img/sconnex.png')}
-                        style={{width: 10, height: 50, marginLeft: 10, marginRight: 10,}}
+                        style={{ width: 10, height: 50, marginLeft: 10, marginRight: 10, }}
                     />
                     <Button
                         title="Inscription"
                         onPress={() => this.props.navigation.navigate('Register')}
-                        color= 'grey'
+                        color='grey'
                     />
                 </View>
-                <View style={{marginBottom: 30,}}>
+                <View style={{ marginBottom: 30, }}>
                     <TextInput
                         style={styles.Input}
                         onChangeText={(text) => this.setState({ pseudo: text, })}
                         placeholder={this.state.pseudo}
                     />
-                    <Image 
+                    <Image
                         source={require('../../img/trait-input.png')}
                         style={styles.trait}
                     />
@@ -61,28 +83,29 @@ export default class Login extends React.Component {
                         onChangeText={(text) => this.setState({ password: text, })}
                         placeholder={this.state.password}
                     />
-                    <Image 
+                    <Image
                         source={require('../../img/trait-input.png')}
                         style={styles.trait}
                     />
                 </View>
                 <View style={styles.check}>
-                    <Text style={{marginTop: 11,}}> Se souvenir de moi </Text>
+                    <Text style={{ marginTop: 11, }}> Se souvenir de moi </Text>
                     <TouchableHighlight
-                    onPress={this.console}
-                    style={{width: 200, height: 20, marginLeft: 20, marginTop: 10,}}
-                >
-                    <View>
-                        <Text style={{fontSize: 15, color: '#000', textAlign: 'center',}}> Mot de passe oublié ? </Text>
-                    </View>
-                </TouchableHighlight>
+                        onPress={this.console}
+                        style={{ width: 200, height: 20, marginLeft: 20, marginTop: 10, }}
+                    >
+                        <View>
+                            <Text style={{ fontSize: 15, color: '#000', textAlign: 'center', }}> Mot de passe oublié ? </Text>
+                        </View>
+                    </TouchableHighlight>
                 </View>
                 <TouchableHighlight
-                    onPress={() => this.props.navigation.navigate('Home')}
+                    //onPress={() => this.props.navigation.navigate('Home')}
+                    onPress={this.connected}
                     style={styles.validate}
                 >
                     <View>
-                        <Text style={{fontSize: 20, color: '#f3f4f3', textAlign: 'center',}}> VALIDER </Text>
+                        <Text style={{ fontSize: 20, color: '#f3f4f3', textAlign: 'center', }}> VALIDER </Text>
                     </View>
                 </TouchableHighlight>
             </View>
@@ -111,12 +134,12 @@ const styles = StyleSheet.create({
         marginBottom: 30,
     },
     Input: {
-        height: 40, 
-        width: 300, 
+        height: 40,
+        width: 300,
         marginTop: 10,
         fontSize: 20,
     },
-    validate:{
+    validate: {
         height: 50,
         width: 220,
         backgroundColor: '#662680',
