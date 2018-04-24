99cfc09e1ef49926a76695bf94feecd6de078df0
Unknown <nelly.lassegue@ynov.com>
2018-04-24 21:20:39 +0200

    feat: page conseil

--- src/pages/routes/conseils.js
+++ src/pages/routes/conseils.js
@@ -1,9 +1,20 @@
-import React, {Component} from 'react'
-import {View, Text, Button, Image, StyleSheet, TouchableHighlight, AppRegistry} from 'react-native'
-import {Icon} from 'react-native-elements'
+import React, { Component } from 'react'
+import { View, ListView, ScrollView, Text, Button, Image, StyleSheet, TouchableHighlight, AppRegistry } from 'react-native'
+import { Icon } from 'react-native-elements'
 import { StackNavigator } from 'react-navigation'
 
 export default class Contact extends React.Component {
+
+
+    constructor(props) {
+        super();
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+        const dataTab = ['Conseils utilisation', 'Conseil alimentaires']
+        this.state = {
+            Name: ds.cloneWithRows(dataTab),
+        };
+    }
+
 
     static navigationOptions = {
         title: 'Conseils pratiques',
@@ -14,29 +25,49 @@ export default class Contact extends React.Component {
         headerTitleStyle: {
             fontSize: 30,
         }
-      };
+    };
 
     render() {
 
         let iconSize = 50
 
-        return(
+        return (
             <View>
-                <View style={{ height: 100, marginTop: '-8%' }}>
-                    <TouchableHighlight style={styles.backButton}>
-                        <Icon
-                            name='navigate-before'
-                            color='#f3f4f3'
-                            size={iconSize}
-                            onPress={() => this.props.navigation.goBack()}
+                <View>
+                    <View style={{ height: 100, marginTop: '-8%' }}>
+                        <TouchableHighlight style={styles.backButton}>
+                            <Icon
+                                name='navigate-before'
+                                color='#f3f4f3'
+                                size={iconSize}
+                                onPress={() => this.props.navigation.goBack()}
+                            />
+                        </TouchableHighlight>
+                        <Image
+                            source={require('../../img/small-couv.png')}
+                            style={styles.header}
                         />
-                    </TouchableHighlight>
-                    <Image
-                        source={require('../../img/fondmenu.png')}
-                        style={styles.FondMenu}
-                    />
-                    <Text style={{ marginTop: -55, marginLeft: '20%', color: '#f3f4f3', fontSize: 30 }}> Conseils pratiques </Text>
+                        <Text style={{ marginTop: -55, marginLeft: '20%', color: '#f3f4f3', fontSize: 30 }}> Conseils pratiques </Text>
+                    </View>
                 </View>
+                <ScrollView style={{ zIndex: 2, }}>
+                    <ListView
+                        dataSource={this.state.Name}
+                        renderRow={(rowName) =>
+                            <View style={styles.case}>
+                                <Text style={{ fontSize: 20, }}>{rowName}</Text>
+                                <View style={{ marginTop: -50, marginLeft: '80%', }}>
+                                    <Icon
+                                        name='navigate-next'
+                                        color='#662680'
+                                        size={iconSize}
+                                        onPress={() => this.props.navigation.goBack()}
+                                    />
+                                </View>
+                            </View>
+                        }
+                    />
+                </ScrollView>
             </View>
         )
     }
@@ -44,10 +75,16 @@ export default class Contact extends React.Component {
 
 const styles = StyleSheet.create({
 
+
+    fond: {
+        backgroundColor: 'red'
+    },
     header: {
         position: 'absolute',
-        width: 400,
-        height: 80,
+        width: '155%',
+        height: '200%',
+        marginTop: '-18%',
+        marginLeft: '-5%',
     },
     container: {
         justifyContent: 'center',
@@ -57,7 +94,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginBottom: '10%',
         zIndex: 5,
-      },
+    },
     text: {
         marginTop: 10,
     },
@@ -73,6 +110,15 @@ const styles = StyleSheet.create({
         zIndex: 1,
 
     },
+    case: {
+        width: '100%',
+        height: 80,
+        borderColor: '#662680',
+        borderBottomWidth: 1,
+        paddingTop: 12,
+        paddingLeft: 10,
+        backgroundColor: '#eef3faff',
+    }
 })
 
 AppRegistry.registerComponent('Contact', () => Contact);
